a9c963691e265bba2c1173f955b29fd4d42a2b43
ayunas <ayunas@gmail.com>
2019-05-17 11:52:01 -0500

    wrote out the helper functions for projects and tasks in one file.

---
+++ helperFunctions.js
@@ -0,0 +1,49 @@
+const db = require('./data/dbConfig');
+
+function getProjects() {
+    return db("projects");
+}
+
+function getAProject(projectID) {
+    return db("projects")
+        .where({id : projectID})
+        .first();
+}
+
+function postProject(proj) {
+    return db("projects")
+            .insert(proj, "id")
+            .then( ([id]) => {    //need help understanding the destructuring and rewriting this explicitly
+                return getAProject(id)
+            });
+};
+
+/************************************************ */
+
+function getTasks() {
+    return db("tasks");
+}
+
+function getATask(taskID) {
+    return db("tasks")
+        .where({id : taskID})
+        .first();
+};
+
+function postTask(task) {
+    return db("tasks")
+            .insert(task, "id")
+            .then( ([id]) => {
+                return getATask(id);
+            });
+};
+
+
+module.exports = {
+    getProjects,
+    getAProject,
+    getTasks,
+    getATask,
+    postProject,
+    postTask
+}
