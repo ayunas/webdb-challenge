72435b27aec38c0662af2f565305508edaabc000
ayunas <ayunas@gmail.com>
2019-05-17 12:07:39 -0500

    created get endpoints for projects

--- projectRouter.js
+++ projectRouter.js
@@ -1,10 +1,33 @@
 const express = require('express')
+const dbHelper = require('./helperFunctions');
 
 const projectRouter = express.Router();
 
 projectRouter.get('/', (req,res) => {
-    res.status(200).json({message : 'welcome to the projectRouter'})
-})
+    //res.status(200).json({message : 'welcome to the projectRouter'})
+    dbHelper.getProjects()
+    .then( projects => {
+        res.status(200).json(projects);
+    })
+    .catch( err => {
+        res.status(500).json(err.message);
+    });
+});
+
+projectRouter.get('/:id', (req,res) => {
+    dbHelper.getAProject(req.params.id)
+        .then( proj => {
+            if (proj) {
+                res.status(200).json(proj);
+            } else {
+                res.status(404).json({message : `the project with id # ${req.params.id} was not found in the database`});
+            }
+        })
+        .catch( err => {
+            res.status(500).json(err.message);
+        })
+});
+
 
 module.exports = projectRouter;
 
