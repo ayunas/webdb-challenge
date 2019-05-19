03cd6b2222cc47532394e0f0b179b3ac235c76af
ayunas <ayunas@gmail.com>
2019-05-19 07:18:22 -0500

    added remaining CRUD endpoints

--- projectRouter.js
+++ projectRouter.js
@@ -1,44 +1,77 @@
-const express = require('express')
-const dbHelper = require('./helperFunctions');
+const express = require("express");
+const dbHelper = require("./helperFunctions");
 
 const projectRouter = express.Router();
 
-projectRouter.get('/', (req,res) => {
-    //res.status(200).json({message : 'welcome to the projectRouter'})
-    dbHelper.getProjects()
-    .then( projects => {
-        res.status(200).json(projects);
+projectRouter.get("/", (req, res) => {
+  //res.status(200).json({message : 'welcome to the projectRouter'})
+  dbHelper
+    .getProjects()
+    .then(projects => {
+      res.status(200).json(projects);
     })
-    .catch( err => {
-        res.status(500).json(err.message);
+    .catch(err => {
+      res.status(500).json(err.message);
     });
 });
 
-projectRouter.get('/:id', (req,res) => {
-    dbHelper.getAProject(req.params.id)
-        .then( proj => {
-            console.log('project', proj);
-            if (proj) {
-                res.status(200).json(proj);
-            } else {
-                res.status(404).json({message : `the project with id # ${req.params.id} was not found in the database`});
-            }
-        })
-        .catch( err => {
-            res.status(500).json(err.message);
+projectRouter.get("/:id", (req, res) => {
+  dbHelper
+    .getAProject(req.params.id)
+    .then(proj => {
+      console.log("project", proj);
+      if (proj) {
+        res.status(200).json(proj);
+      } else {
+        res.status(404).json({
+          message: `the project with id # ${
+            req.params.id
+          } was not found in the database`
         });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
 });
 
-projectRouter.post('/', (req,res) => {
-    dbHelper.postProject(req.body)
-        .then( proj => {
-            res.status(201).json(proj);
-        })
-        .catch(err => {
-            res.status(500).json(err.message);
-        });
+projectRouter.post("/", (req, res) => {
+  dbHelper
+    .postProject(req.body)
+    .then(proj => {
+      res.status(201).json(proj);
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
 });
 
+projectRouter.put("/:id", (req, res) => {
+  dbHelper
+    .putProject(req.body, req.params.id)
+    .then(proj => {
+      res.status(200).json(proj);
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
+});
+
+projectRouter.delete("/:id", (req, res) => {
+  dbHelper
+    .deleteProject(req.params.id)
+    .then(count => {
+      res
+        .status(204)
+        .json(
+          `${count} project with id # ${
+            req.params.id
+          } was deleted from the database`
+        );
+    })
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
+});
 
 module.exports = projectRouter;
-
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 const bodyParser = express.json();
-const projectRouter = require('./projectRouter');
-const taskRouter = require('./taskRouter');
+const projectRouter = require("./projectRouter");
+const taskRouter = require("./taskRouter");
 
 server.use(bodyParser);
 
-server.get('/', (req,res) => {
-    res.status(200).send('`<h2>Welcome to Projects</h2>');
-})
+server.get("/", (req, res) => {
+  res.status(200).send("`<h2>Welcome to Projects</h2>");
+});
 
-server.use('/projects', projectRouter);
-server.use('/tasks', taskRouter);
+server.use("/projects", projectRouter);
+server.use("/tasks", taskRouter);
 
 module.exports = server;
-
